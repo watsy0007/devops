24c838b094721806b89d24d48a16c7fab18d71cb
Boris Ding <shouxian@opifices.com>
2015-09-11 01:19:44 +0800

    Update coreos.sh

--- server_init/coreos.sh
+++ server_init/coreos.sh
@@ -7,7 +7,7 @@ if [ "$(id -u)" != "0" ]; then
     echo "This script must be run as root" 1>&2
     exit 1
 fi
-touch ./ubuntu_trusty.sh
+bash ./ubuntu_trusty.sh
 apt-get -y purge tcpdump nano ufw openssh-sftp-server
 curl -sSL https://get.daocloud.io/docker | sh
 echo "Inpur Daocloud Key:"
