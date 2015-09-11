f529880d81736f2f12a87bc3a1426b618e13d4b7
Boris Ding <shouxian@opifices.com>
2015-09-11 17:10:25 +0800

    Update coreos.sh

--- server_init/coreos.sh
+++ server_init/coreos.sh
@@ -10,6 +10,8 @@ fi
 bash ./ubuntu_trusty.sh
 apt-get -y purge tcpdump nano ufw
 curl -sSL https://get.daocloud.io/docker | sh
+service docker stop
+echo "DOCKER_OPTS=\"-g /alidata1\"" > /etc/default/docker
 echo "Inpur Daocloud Key:"
 read key
 curl -sSL https://get.daocloud.io/daomonit/install.sh | sh -s $key
