0379da2881c72f524bb5ff0b853f613f45f320a2
Boris Ding <shouxian@opifices.com>
2016-03-07 11:16:02 +0800

    Create jenkis_slave.sh

---
+++ server_init/jenkis_slave.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+if [ $(lsb_release -c -s) != "trusty" ]; then
+    echo "This script must be run as Ubuntu Trusty" 1>&2
+    exit 1
+fi
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+bash ./ubuntu_trusty.sh
+add-apt-repository -y ppa:webupd8team/java
+aptitude update
+aptitude install -y oracle-java8-install oracle-java8-set-default graphicsmagick imagemagick ghostscript libgs-dev pdftk poppler-utils libpoppler-dev monit cron libmysqlclient-dev mysql-client libpq-dev postgresql-client gcc cmake g++ zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libxml2-dev libxslt1-dev libcur14-openssl-dev libffi-dev libpcre3-dev libjemalloc-dev gawk autoconf libgdbm-dev libncurses5-dev automake libtool bison pkg-config
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+source ~/.profile
+nvm install 5.2.0
+nvm alias default 5.2.0
+nvm use default
+npm install -g cnpm 
+cnpm install -g bower cordova ionic grunt yo
+\curl -L https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+rvm requirements
+rvm install ruby-2.3.0
+rvm use ruby-2.3.0 --default
+gem sources --add https://ruby.taobao.org/ --remove https://rubygems.org/
+gem install bundler 
+
