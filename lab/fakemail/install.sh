b24234f8dde509c710f5edcbfebad801948c8a3a
Ruijian Cao <cao7113@hotmail.com>
2016-07-27 12:11:32 +0800

    add fakemail docker

---
+++ lab/fakemail/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+apt-get update && apt-get install -y build-essential libsqlite3-dev sqlite3
+
+gem install mailcatcher --no-ri --no-rdoc
+
+# cleanup package manager
+apt-get remove --purge -y build-essential libsqlite3-dev 
+apt-get autoclean && apt-get clean
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
