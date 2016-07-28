196defb6266da1dbaaaf81bf37a3c0a385c5f0c4
Ruijian Cao <cao7113@hotmail.com>
2016-07-28 14:00:10 +0800

    fix

--- lab/syslog/client/try/syslog.rb
+++ lab/syslog/client/try/syslog.rb
@@ -2,9 +2,10 @@ require 'byebug'
 require 'syslog/logger'
 
 # 注： 仅default名字起作用
-l = Syslog::Logger.new('default')
 l0 = Syslog::Logger.new('local0', Syslog::LOG_LOCAL0)
 l1 = Syslog::Logger.new('local1', Syslog::LOG_LOCAL1)
+l2 = Syslog::Logger.new('local2', Syslog::LOG_LOCAL2)
+l3 = Syslog::Logger.new('local3', Syslog::LOG_LOCAL3)
 
 puts '==try syslog here'
 byebug
